7b54eae88dbed0141948be6bbf832f3a96e14b84
Jonathan Slack <jonathan.m.slack@outlook.com>
2020-03-09 11:00:00 -0400

    Add project files.

---
+++ SimonSays/Display.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using Console = Colorful.Console;
+
+
+namespace SimonSays
+{
+    public class Display
+    {
+
+        public int DisplayMenu(int exit)
+        {
+            List<string> menu = new List<string>() { "Start Game", "HighScore", "Controls", "Exit Game"};
+            int currentTask = 0;
+            bool menuLoop = false;
+
+            do
+            {
+
+
+                for (int i = 0; i < menu.Count; i++)
+                {
+                    if (i == currentTask)
+                    {
+                        Console.BackgroundColor = Color.White;
+                        Console.ForegroundColor = Color.Black;
+                        Console.SetCursorPosition(54, (21 + i));
+                        System.Console.WriteLine($"{menu[i]}");
+                        Console.BackgroundColor = Color.Black;
+                        Console.ForegroundColor = Color.White;
+
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(54, (21 + i));
+                        System.Console.WriteLine(menu[i]);
+
+                    }
+                }
+
+                    var keyInfo = Console.ReadKey(true).Key;
+                    switch (keyInfo)
+                    {
+                        case ConsoleKey.UpArrow:
+                            {
+                                if (currentTask != 0)
+                                {
+                                    currentTask--;
+                                }
+                                else
+                                {
+
+                                    currentTask = 3;
+
+                                }
+                                break;
+
+                            }
+                        case ConsoleKey.DownArrow:
+                            {
+                                if (currentTask != 3)
+                                {
+                                    currentTask++;
+                                }
+                                else
+                                {
+
+                                    currentTask = 0;
+
+                                }
+                            break;
+
+                            }
+                        case ConsoleKey.Enter:
+                            {
+                                if (currentTask == 0)
+                                {
+                                Console.Clear();
+                                menuLoop = true;
+                                exit = 0;
+                                }
+                                else if (currentTask == 1)
+                                {
+                                exit = 1;
+                                }
+                                else if (currentTask == 2)
+                                {
+                                menuLoop = true;
+                                exit = 2; 
+                                }
+                                else if(currentTask == 3)
+                                {
+                                menuLoop = true;
+                                exit = 3;
+                                }
+                                break;
+
+                            }
+
+
+                    }
+                
+            } while (!menuLoop);
+            return exit;
+        }
+
+
+
+        public void DisplayName()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.WriteAscii("    CORTANA SAYS", Color.FromArgb(214, 170, 255));
+        }
+       public void DisplayTiles()
+        {
+            Console.SetCursorPosition(54, 12);
+            Console.BackgroundColor = Color.DarkRed;
+            Console.WriteLine("    ");
+            Console.SetCursorPosition(54, 13);
+            Console.BackgroundColor = Color.DarkRed;
+            Console.WriteLine("    ");
+            Console.ResetColor();
+            Console.SetCursorPosition(60, 12);
+            Console.BackgroundColor = Color.DarkGreen;
+            Console.WriteLine("    ");
+            Console.SetCursorPosition(60, 13);
+            Console.BackgroundColor = Color.DarkGreen;
+            Console.WriteLine("    ");
+            Console.ResetColor();
+            Console.SetCursorPosition(54, 15);
+            Console.BackgroundColor = Color.DarkBlue;
+            Console.WriteLine("    ");
+            Console.SetCursorPosition(54, 16);
+            Console.BackgroundColor = Color.DarkBlue;
+            Console.WriteLine("    ");
+            Console.ResetColor();
+            Console.SetCursorPosition(60, 15);
+            Console.BackgroundColor = Color.DarkGoldenrod;
+            Console.WriteLine("    ");
+            Console.SetCursorPosition(60, 16);
+            Console.BackgroundColor = Color.DarkGoldenrod;
+            Console.WriteLine("    ");
+            Console.ResetColor();
+        }
+
+        public void IlluminateTile(int tile)
+        {
+            if (tile == 0)
+            {
+                Console.SetCursorPosition(54, 12);
+                Console.BackgroundColor = Color.Red;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(54, 13);
+                Console.BackgroundColor = Color.Red;
+                Console.WriteLine("    ");
+                Console.Beep(330, 400);
+                Thread.Sleep(200);
+                Console.SetCursorPosition(54, 12);
+                Console.BackgroundColor = Color.DarkRed;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(54, 13);
+                Console.BackgroundColor = Color.DarkRed;
+                Console.WriteLine("    ");
+                Thread.Sleep(100);
+
+
+            }
+            else if (tile == 1)
+            {
+                Console.SetCursorPosition(60, 12);
+                Console.BackgroundColor = Color.Green;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(60, 13);
+                Console.BackgroundColor = Color.Green;
+                Console.WriteLine("    ");
+                Console.Beep(220, 400);
+                Thread.Sleep(200);
+                Console.SetCursorPosition(60, 12);
+                Console.BackgroundColor = Color.DarkGreen;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(60, 13);
+                Console.BackgroundColor = Color.DarkGreen;
+                Console.WriteLine("    ");
+                Thread.Sleep(100);
+
+            }
+            else if (tile == 2)
+            {
+                Console.SetCursorPosition(54, 15);
+                Console.BackgroundColor = Color.Blue;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(54, 16);
+                Console.BackgroundColor = Color.Blue;
+                Console.WriteLine("    ");
+                Console.Beep(277, 400);
+                Thread.Sleep(200);
+                Console.SetCursorPosition(54, 15);
+                Console.BackgroundColor = Color.DarkBlue;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(54, 16);
+                Console.BackgroundColor = Color.DarkBlue;
+                Console.WriteLine("    ");
+                Thread.Sleep(100);
+
+
+            }
+            else
+            {
+                Console.SetCursorPosition(60, 15);
+                Console.BackgroundColor = Color.Yellow;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(60, 16);
+                Console.BackgroundColor = Color.Yellow;
+                Console.WriteLine("    ");
+                Console.Beep(392, 400);
+                Thread.Sleep(200);
+                Console.SetCursorPosition(60, 15);
+                Console.BackgroundColor = Color.DarkGoldenrod;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(60, 16);
+                Console.BackgroundColor = Color.DarkGoldenrod;
+                Console.WriteLine("    ");
+                Thread.Sleep(100);
+
+
+            }
+        }
+       public void UserIlluminate(int tile)
+       {
+                if (tile == 0)
+                {
+                Console.SetCursorPosition(54, 12);
+                Console.BackgroundColor = Color.Red;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(54, 13);
+                Console.BackgroundColor = Color.Red;
+                Console.WriteLine("    ");
+                Console.Beep(330, 400);
+                Console.SetCursorPosition(54, 12);
+                Console.BackgroundColor = Color.DarkRed;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(54, 13);
+                Console.BackgroundColor = Color.DarkRed;
+                Console.WriteLine("    ");
+
+            }
+                else if (tile == 1)
+            {
+                Console.SetCursorPosition(60, 12);
+                Console.BackgroundColor = Color.Green;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(60, 13);
+                Console.BackgroundColor = Color.Green;
+                Console.WriteLine("    ");
+                Console.Beep(220, 400);
+                Console.SetCursorPosition(60, 12);
+                Console.BackgroundColor = Color.DarkGreen;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(60, 13);
+                Console.BackgroundColor = Color.DarkGreen;
+                Console.WriteLine("    ");
+            }
+                else if (tile == 2)
+                {
+                Console.SetCursorPosition(54, 15);
+                Console.BackgroundColor = Color.Blue;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(54, 16);
+                Console.BackgroundColor = Color.Blue;
+                Console.WriteLine("    ");
+                Console.Beep(277, 400);
+                Console.SetCursorPosition(54, 15);
+                Console.BackgroundColor = Color.DarkBlue;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(54, 16);
+                Console.BackgroundColor = Color.DarkBlue;
+                Console.WriteLine("    ");
+
+            }
+                else
+                {
+                Console.SetCursorPosition(60, 15);
+                Console.BackgroundColor = Color.Yellow;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(60, 16);
+                Console.BackgroundColor = Color.Yellow;
+                Console.WriteLine("    ");
+                Console.Beep(392, 400);
+                Console.SetCursorPosition(60, 15);
+                Console.BackgroundColor = Color.DarkGoldenrod;
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(60, 16);
+                Console.BackgroundColor = Color.DarkGoldenrod;
+                Console.WriteLine("    ");
+
+            }
+            }
+
+        public void ControlMarkings()
+        {
+            Console.ForegroundColor = Color.White;
+            Console.BackgroundColor = Color.Black;
+            Console.SetCursorPosition(55, 12);
+            System.Console.WriteLine("I");
+            Console.SetCursorPosition(61, 12);
+            System.Console.WriteLine("O");
+            Console.SetCursorPosition(55, 15);
+            System.Console.WriteLine("K");
+            Console.SetCursorPosition(61, 15);
+            System.Console.WriteLine("L");
+            Console.SetCursorPosition(50, 27);
+            Console.WriteLine("Press 'E' to Exit");
+ 
+
+        }
+
+    }
+}
---
+++ SimonSays/Program.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+using System.Threading;
+
+
+namespace SimonSays
+{
+    class Program
+    {
+    
+
+        static void Main(string[] args)
+        {
+          
+
+            Program p = new Program();
+            p.GameEngine();            
+        }
+
+        public void GameEngine()
+        {
+            //List<int> compTile = new List<int>();
+            //int listLength = compTile.Count;
+            Console.CursorVisible = false;
+            bool replay = false;
+            Random prng = new Random();
+            Display d = new Display();
+            
+
+
+            Point Point1 = new Point(0, 0);
+            ConsoleRectangle c = new ConsoleRectangle(117, 27, Point1, ConsoleColor.Cyan);
+            bool menuLoop = false;
+
+            int scoreCounter = 0;
+
+            Display m = new Display();
+
+            do
+            {
+                Console.Clear();
+                m.DisplayName();
+                //c.DrawBox();
+                d.DisplayTiles();
+                int exit = m.DisplayMenu(0);
+                switch(exit)
+                {
+                    case 0: {
+
+                        do
+                        {
+                            List<int> compTile = new List<int>();
+                            int listLength = compTile.Count;
+                            Console.CursorVisible = false;
+                            bool gameLoop = false;
+                            scoreCounter = 0;
+
+                            Console.SetCursorPosition(50, 27);
+                            Console.WriteLine("Press 'E' to Exit");
+                            m.DisplayName();
+                            //c.DrawBox();
+                            d.DisplayTiles();
+
+                            do
+                            {
+                                Thread.Sleep(400);
+                                foreach (int i in compTile)
+                                {
+                                    Thread.Sleep(200);
+                                    d.IlluminateTile(i);
+
+                                }
+
+                                int tile = prng.Next(0, 4);
+
+                                if (tile == 0)
+                                {
+                                    d.IlluminateTile(tile);
+                                    compTile.Add(tile);
+
+
+                                }
+                                else if (tile == 1)
+                                {
+                                    d.IlluminateTile(tile);
+                                    compTile.Add(tile);
+
+                                }
+                                else if (tile == 2)
+                                {
+                                    d.IlluminateTile(tile);
+                                    compTile.Add(tile);
+
+                                }
+                                else
+                                {
+                                    d.IlluminateTile(tile);
+                                    compTile.Add(tile);
+
+                                }
+
+                                listLength = compTile.Count;
+                                for (int i = 0; i < listLength; i++)  // Waiting for User Response
+                                {
+                                    int listTile = compTile[i];
+
+                                    switch (listTile)
+                                    {
+                                        case 0:
+                                            {
+                                                var userInput = Console.ReadKey(true).Key;
+                                                if (userInput == ConsoleKey.I)
+                                                {
+                                                    d.UserIlluminate(listTile);
+
+                                                    break;
+
+                                                }
+                                                else
+                                                {
+                                                    Console.Beep(700, 1000);
+                                                    i = listLength;
+                                                    gameLoop = true;
+
+                                                    break;
+                                                }
+
+                                            }
+                                        case 1:
+                                            {
+                                                var userInput = Console.ReadKey(true).Key;
+                                                if (userInput == ConsoleKey.O)
+                                                {
+                                                    d.UserIlluminate(listTile);
+
+                                                    break;
+
+                                                }
+                                                else
+                                                {
+                                                    Console.Beep(700, 1000);
+                                                    i = listLength;
+                                                    gameLoop = true;
+                                                    break;
+                                                }
+                                            }
+                                        case 2:
+                                            {
+                                                var userInput = Console.ReadKey(true).Key;
+                                                if (userInput == ConsoleKey.K)
+                                                {
+                                                    d.UserIlluminate(listTile);
+
+                                                    break;
+
+                                                }
+                                                else
+                                                {
+                                                    Console.Beep(700, 1000);
+                                                    i = listLength;
+                                                    gameLoop = true;
+                                                    break;
+                                                }
+                                            }
+                                        case 3:
+                                            {
+                                                var userInput = Console.ReadKey(true).Key;
+                                                if (userInput == ConsoleKey.L)
+                                                {
+                                                    d.UserIlluminate(listTile);
+
+                                                    break;
+
+                                                }
+                                                else
+                                                {
+                                                    Console.Beep(700, 1000);
+                                                    i = listLength;
+                                                    gameLoop = true;
+                                                    break;
+                                                }
+                                            }
+                                        case 4:
+                                            {
+                                                var userInput = Console.ReadKey(true).Key;
+                                                if (userInput == ConsoleKey.E)
+                                                {
+                                                    break;
+                                                }
+                                                break;
+                                            }
+                                    }
+
+                                }
+
+
+
+                                scoreCounter++;
+
+
+                            } while (!gameLoop);
+
+                            Console.SetCursorPosition(47, 23);
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.WriteLine($"GAME OVER! Your Score: {scoreCounter - 1}");
+                            Console.SetCursorPosition(47, 24);
+                            Console.WriteLine("Play Again?: Y / N");
+
+                            var playAgain = Console.ReadKey(true).Key;
+                            switch (playAgain)
+                            {
+                                case ConsoleKey.Y:
+                                    {
+                                        Console.Clear();
+                                        break;
+                                    }
+                                case ConsoleKey.N:
+                                    {
+                                        replay = true;
+                                        break;
+                                    }
+
+
+                            }
+                        } while (!replay);
+                        break;
+                    }
+                    case 1:
+                        {
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Clear();
+                            m.DisplayName();                           
+                            d.DisplayTiles();                   
+                            d.ControlMarkings();
+                            var userInput = Console.ReadKey(true).Key;
+                            if (userInput == ConsoleKey.E)
+                            {
+                               
+                                break;
+
+                            }
+
+                            break;
+                        }
+                    case 3:
+                        {
+                            menuLoop = true;
+                            break;
+                        }
+
+                }
+
+            } while (!menuLoop);
+        }
+
+    }
+}
